c21227cc9c44fd3127ac637c22b7c5e4c29cdcaa
ACEsuit <christohortner@gmail.com>
2024-05-27 12:06:34 -0700

    second ss ex, bug in EP?

--- test/test_saddle_search.jl
+++ test/test_saddle_search.jl
@@ -18,6 +18,7 @@ Random.seed!(100)
 # sw = StillingerWeber() 
 # ucell1, _ = GO.minimise(ucell0, sw; g_tol = 1e-5, variablecell=true)
 
+
 sys0 = AosSystem( bulk(:Si, cubic=true) * 2 )
 # sys0[2] is a neighbour of sys0[1] 
 @assert norm(position(sys0, 1)) == 0u"Å"
@@ -38,3 +39,45 @@ v0 = x1 - x0  # initial rotation
 
 dimer = StaticDimer(a_trans=0.0001, a_rot=0.0001, len=1e-3, maxnumdE=3000)
 x, v, report = SaddleSearch.run!(dimer, obj_f, obj_g, x1, v0)
+
+
+#
+
+# first get the ground state unit cell 
+# this needs some fixes in DP
+# ucell0 = AosSystem(bulk(:Si, cubic=true))
+# sw = StillingerWeber() 
+# ucell1, _ = GO.minimise(ucell0, sw; g_tol = 1e-5, variablecell=true)
+
+r0 = AtomsBuilder.Chemistry.rnn(:Cu)
+sys0 = AosSystem( bulk(:Cu, cubic=true) * 3 )
+# x2 is a nearest-neighbour of x1
+@assert norm(position(sys0, 1)) == 0u"Å"
+@assert norm(position(sys0, 2)) <= 1.0001 * r0
+𝐫2 = position(sys0, 2)
+# delete the second atom and move at1 onto the midpoint 
+# the new system sys1 is an initial guess for a saddle point 
+deleteat!(sys0.particles, 2)
+sys1 = SoaSystem(sys0)
+set_position!(sys1, 1, 0.5 * 𝐫2)
+
+# LJ is not a good model for Cu, but a perfectly ok model for a generic 
+# FCC crystal at 0T 
+zU = sys0[1].𝑍
+calc = LennardJones(-1.0u"eV", r0, zU, zU, 3*r0,)
+
+# create a system -> dof mapping 
+dofmgr1 = GeomOpt.DofManager(sys1; variablecell=false)
+obj_f, obj_g = GeomOpt.get_obj_fg(sys1, calc, dofmgr1)
+x1 = GeomOpt.get_dofs(sys1, dofmgr1)
+
+# to get the initial orientation of the dimer, we get the reference position 
+# first and then take the difference 
+x0 = GeomOpt.get_dofs(sys0, dofmgr1)
+v0 = x1 - x0  # initial rotation 
+
+dimer = StaticDimer(a_trans=0.001, a_rot=0.001, len=1e-3, maxnumdE=3000)
+x, v, report = SaddleSearch.run!(dimer, obj_f, obj_g, x1, v0)
+
+obj_f(x1)
+norm(obj_g(x1))
